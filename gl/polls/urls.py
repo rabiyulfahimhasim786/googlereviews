8f20edd617d926000e5936c930f5a5c6df038e8e
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2022-10-15 07:57:03 -0500

    Add files via upload

--- gl/polls/urls.py
+++ gl/polls/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('reviews/', views.reviews, name='reviews'),
+    path('woodlands/', views.woodlandsreviews, name='woodlandsreviews'),
+    path('humble/', views.humblereviews, name='humblereviews'),
+    path('houston/', views.houstonreviews, name='houstonreviews'),
     path('gmaps/', views.gmaps, name='gmaps'),
 ]
--- gl/polls/views.py
+++ gl/polls/views.py
@@ -24,7 +24,7 @@ def reviews(request):
     with open('./media/json/data.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
     # Opening JSON file
-    with open('./media/json/data.json', 'r') as openfile:
+    with open('./media/json/data.json', 'r', encoding='utf8') as openfile:
         # Reading from json file
         json_object = json.load(openfile)
     #print(json_object)
@@ -36,8 +36,7 @@ def reviews(request):
     return HttpResponse("Hello, world!")
 
 def gmaps(request):
-    gmaps = googlemaps.Client(key='INSERT_KEY_HERE')
-
+    #gmaps = googlemaps.Client(key='INSERT_KEY_HERE')
     place_name = 'Desss applying technology, 12th Street, C-Sector, Anna Nagar West Extension, Chennai, Tamil Nadu, India'
     places_result = gmaps.places(place_name)
     place_id = places_result['results'][0]['place_id']
@@ -59,4 +58,61 @@ def gmaps(request):
     df = pd.DataFrame(reviews)
     #df
     df.to_csv('./media/csv/desss_reviews.csv', index=False)
+    return HttpResponse("Hello, world!")
+
+
+def woodlandsreviews(request):
+    r = requests.get('https://api.reviewsmaker.com/gmb/?placeid=ChIJ14TUqeg2R4YRBrqlE90VPCI')
+    woodlandsdata = r.json()
+    #print(data)
+    with open('./media/json/woodlandsdata.json', 'w', encoding='utf-8') as f:
+        json.dump(woodlandsdata, f, ensure_ascii=False, indent=4)
+    # Opening JSON file
+    with open('./media/json/woodlandsdata.json', 'r', encoding='utf-8') as openwoodlandsfile:
+        # Reading from json file
+        woodlands_object = json.load(openwoodlandsfile)
+    #print(json_object)
+    #print(type(json_object))
+    wdatas = json.load(open('./media/json/woodlandsdata.json', 'r', encoding='utf-8'))
+    #pd.DataFrame(data["reviews"])
+    wdf = pd.DataFrame(wdatas["reviews"])
+    wdf.to_csv('./media/csv/woodlandsdata.csv', encoding='utf-8', index=False)
+    return HttpResponse("Hello, world!")
+
+
+def humblereviews(request):
+    r = requests.get('https://api.reviewsmaker.com/gmb/?placeid=ChIJ2dMVZbSzQIYRp67xsoqHLq0')
+    humbledata = r.json()
+    #print(data)
+    with open('./media/json/humbledata.json', 'w', encoding='utf-8') as f:
+        json.dump(humbledata, f, ensure_ascii=False, indent=4)
+    # Opening JSON file
+    with open('./media/json/humbledata.json', 'r', encoding='utf-8') as openhumblefile:
+        # Reading from json file
+        humble_object = json.load(openhumblefile)
+    #print(json_object)
+    #print(type(json_object))
+    hdatas = json.load(open('./media/json/humbledata.json', 'r', encoding='utf-8'))
+    #pd.DataFrame(data["reviews"])
+    hdf = pd.DataFrame(hdatas["reviews"])
+    hdf.to_csv('./media/csv/humbledata.csv', encoding='utf-8', index=False)
+    return HttpResponse("Hello, world!")
+
+
+def houstonreviews(request):
+    r = requests.get('https://api.reviewsmaker.com/gmb/?placeid=ChIJodhRBeDKQIYRUSg7EtP6VvA')
+    houstondata = r.json()
+    #print(data)
+    with open('./media/json/houstondata.json', 'w', encoding='utf-8') as f:
+        json.dump(houstondata, f, ensure_ascii=False, indent=4)
+    # Opening JSON file
+    with open('./media/json/houstondata.json', 'r', encoding='utf-8') as openhoustonfile:
+        # Reading from json file
+        houston_object = json.load(openhoustonfile)
+    #print(json_object)
+    #print(type(json_object))
+    houstondatas = json.load(open('./media/json/houstondata.json', 'r', encoding='utf-8'))
+    #pd.DataFrame(data["reviews"])
+    houstondf = pd.DataFrame(houstondatas["reviews"])
+    houstondf.to_csv('./media/csv/houstondata.csv', encoding='utf-8', index=False)
     return HttpResponse("Hello, world!")
